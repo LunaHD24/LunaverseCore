770eb01903492f002a35cf1af0c0671d0a476997
LunaHD24 <yannick.rumelfanger@gmail.com>
2025-04-25 12:23:14 +0200

    Removed reflections dependency

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -13,7 +13,6 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
-    implementation("org.reflections:reflections:0.10.2")
     implementation(project(":api"))
 }
 
--- plugin/src/main/java/dev/lunaa/lunaversecore/api/ApiServiceInitializer.java
+++ plugin/src/main/java/dev/lunaa/lunaversecore/api/ApiServiceInitializer.java
@@ -1,25 +1,11 @@
 package dev.lunaa.lunaversecore.api;
 
-import dev.lunaa.lunaversecore.LunaverseCore;
-import org.reflections.Reflections;
-
-import java.util.Set;
+import dev.lunaa.lunaversecore.api.registry.RegistryService;
 
 public class ApiServiceInitializer {
 
     public static void initialize() {
-        Reflections reflections = new Reflections("dev.lunaa.lunaversecore.api");
-        Set<Class<? extends ApiService>> apiServices = reflections.getSubTypesOf(ApiService.class);
-
-        for (Class<? extends ApiService> apiService : apiServices) {
-            try {
-                ApiService service = apiService.getDeclaredConstructor().newInstance();
-                service.initialize();
-            } catch (Exception e) {
-                LunaverseCore.getLunaLogger().error("Failed to initialize API service: " + apiService.getName());
-                e.printStackTrace();
-            }
-        }
+        new RegistryService().initialize();
     }
 
 }
